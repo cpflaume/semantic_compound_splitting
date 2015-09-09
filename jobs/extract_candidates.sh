20e4c84bf9826a753a13b900c482a00b07468b46
mtm027 <mtm027@pandora1.ufal.hide.ms.mff.cuni.cz>
2015-09-09 19:13:30 +0200

    More cleanup++

---
+++ jobs/extract_candidates.sh
@@ -0,0 +1,13 @@
+# shell for the job:
+#PBS -S /bin/bash
+#PBS -lnodes=1:cores16:mem64gb
+#PBS -lwalltime=48:00:00
+
+cd $CODE_DIR
+
+# venv
+module load python  2>err_py
+
+$PYTHON extract_candidates.py -w $OUT_DIR/w2v.bin -b $OUT_DIR/dawg -c $OUT_DIR/candidates -o $OUT_DIR/annoy_index -i $OUT_DIR/candidates_index.p -l $MIN_LENGTH -n 100 -f "$FUGENELEMENTE"
+
+touch $OUT_DIR/step2.done
