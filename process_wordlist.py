ad60b0b7dbde4c5807e2c71e76ae7a89ec21a8e7
J. Daiber <jdaiber1@laco12.science.uva.nl>
2015-09-15 17:09:33 +0200

    Processing word lists.

---
+++ process_wordlist.py
@@ -0,0 +1,17 @@
+import decompound_annoy
+from compound import Compound
+from lattice import Lattice
+import fileinput
+import argparse
+
+def lattice(c):
+    return decompound_annoy.get_decompound_lattice(c, 250, 0.0)
+
+def viterbi(c):
+    return decompound_annoy.vit.viterbi_decode(Compound(c, None,
+        Lattice(lattice(c))))
+
+for line in fileinput.input():
+    c = line.strip()
+    print " ".join(map(lambda p: "%d,%d" % p, viterbi(c)))
+
