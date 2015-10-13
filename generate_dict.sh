3103fc384772ad4d47c742d0c7ecb2c6f11a8bcc
Joachim Daiber <jdaiber@test3.irc.sara.nl.irc.sara.nl>
2015-10-13 13:55:40 +0200

    Incremental dict generation.

--- generate_dict.sh
+++ generate_dict.sh
@@ -4,11 +4,22 @@ export COMPOUND_MODEL="/home/jdaiber/compounds/de.model"
 export PYTHON="/home/jdaiber/compounds/semantic_compound_splitting/venv/bin/python"
 export CODE_DIR="/home/jdaiber/compounds_dict/semantic_compound_splitting"
 
-#python decompound.py /tmp/compound_model/ --mode w2v_dict > de.words
+#$PYTHON decompound.py /tmp/compound_model/ --mode w2v_dict > de.words
+
 #mkdir de_words
 #split -l 32000 de.words de_words/words
 
-for p in de_words/*; do
+all_jobs=""
+
+for p in $(ls de_words/* | grep -v dict); do
+    if [[ -s "${p}.dict" ]]; then
+        :
+    else
+        all_jobs="$all_jobs $p"
+    fi
+done
+
+for p in $all_jobs; do
     qsub -v CODE_DIR,PYTHON,COMPOUND_MODEL,PART=$p jobs/generate_dict.sh
 done
 
