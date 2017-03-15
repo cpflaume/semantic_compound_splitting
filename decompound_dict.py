7bdb6a6683b1532b82bebf3bfc4ea37bc5973868
Jo Daiber <jodaiber@gmx.de>
2017-03-15 14:59:35 -0700

    Added --ignore_case flag.

--- decompound_dict.py
+++ decompound_dict.py
@@ -4,12 +4,16 @@ import sys
 import fileinput
 import argparse
 
-def load_dict(file):
+def load_dict(file, ignore_case=False):
     splits = {}
     with open(file) as f:
         for line in f:
             es = line.decode('utf8').rstrip('\n').split(" ")
             w = es[0]
+            if args.ignore_case:
+                # TODO, always using the last one in case of overlaps
+                w = w.lower()
+            
             indices = map(lambda i: i.split(','), es[1:])
 
             splits[w] = []
@@ -28,12 +32,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Decompound words.')
     parser.add_argument('dict')
     parser.add_argument('--drop_fugenlaute', help='If this flag is set, Fugenlaute (infixes such as -s, -es) are dropped from the final words.', action='store_true')
-    parser.add_argument('--lowercase', help='Lowercase all words.', action='store_true')
-    parser.add_argument('--restore_case', help='Restore the case (words will take case of the original word).', default=True)
+    parser.add_argument('--lowercase', help='Lowercase all output words.', action='store_true')
+    parser.add_argument('--ignore_case', help='Ignore upper/lowercase (words passed should be all lowercase)', action='store_true', default=False)
+    parser.add_argument('--restore_case', help='Restore the case (words will take case of the original word).', action='store_true', default=True)
     args = parser.parse_args()
 
-    splits = load_dict(args.dict)
+    splits = load_dict(args.dict, ignore_case=args.ignore_case)
     def split_word(w):
+        if args.ignore_case:
+            w = w.lower()
+        
         if w in splits:
 
             w_split = []
